f5714dd234bcc73b2bb58a6770f8c178187a14c4
YounByoungil <jimmy7525@naver.com>
2021-07-31 23:06:51 +0900

    Set SceneDelegate

---
+++ UberProject/UberProject/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  UberProject
+//
+//  Created by 윤병일 on 2021/07/31.
+//
+
+import UIKit
+
+class LoginController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
