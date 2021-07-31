d5f9abe9fda2828f210db6042249c0f0a7503606
YounByoungil <jimmy7525@naver.com>
2021-08-01 00:08:01 +0900

    Programmatic Intro To UI

--- UberProject/UberProject/Controller/LoginController.swift
+++ UberProject/UberProject/Controller/LoginController.swift
@@ -11,9 +11,35 @@ class LoginController : UIViewController {
   
   //MARK: - Properties
   
+  private let titleLabel : UILabel = {
+    let label = UILabel()
+    label.text = "UBER"
+    label.font = UIFont(name: "Avenir-Light", size: 36)
+    label.textColor = UIColor(white: 1, alpha: 0.8)
+    return label
+  }()
+  
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .red
+    configureUI()
+  }
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = UIColor(red: 25 / 255, green: 25 / 255, blue: 25 / 255, alpha: 1)
+    
+    [titleLabel].forEach {
+      view.addSubview($0)
+    }
+    
+    titleLabel.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      $0.centerX.equalToSuperview()
+    }
   }
 }
