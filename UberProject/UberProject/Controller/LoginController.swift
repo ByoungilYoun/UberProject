9139fb2f1f71d6e9997cd61ac4715a0a0fd87b79
YounByoungil <jimmy7525@naver.com>
2021-08-07 01:45:52 +0900

    Log User in

--- UberProject/UberProject/Controller/LoginController.swift
+++ UberProject/UberProject/Controller/LoginController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginController : UIViewController {
   
@@ -47,6 +48,7 @@ class LoginController : UIViewController {
     let button = AuthButton(type: .system)
     button.setTitle("Log In", for: .normal)
     button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+    button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
     return button
   }()
   
@@ -107,5 +109,19 @@ class LoginController : UIViewController {
   @objc func handleShowSignUp() {
     let controller = SignUpController()
     navigationController?.pushViewController(controller, animated: true)
+  }
+  
+  @objc func handleLogin() {
+    guard let email = emailTextField.text else {return}
+    guard let password = passwordTextField.text else {return}
+    
+    Auth.auth().signIn(withEmail: email, password: password) { result, error in
+      if let error = error {
+        print("Failed to log user in with error \(error.localizedDescription)")
+        return
+      }
+      
+      print("Successfully log user in")
+    }
   }
 }
