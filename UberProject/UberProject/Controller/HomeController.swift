2526c13954b4c274a1dfb1d3fd401411425edd51
YounByoungil <jimmy7525@naver.com>
2021-08-08 00:38:53 +0900

    Requesting User Location

--- UberProject/UberProject/Controller/HomeController.swift
+++ UberProject/UberProject/Controller/HomeController.swift
@@ -14,11 +14,13 @@ class HomeController : UIViewController {
   //MARK: - Properties
   
   private let mapView = MKMapView()
+  private let locationManager = CLLocationManager()
   
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     checkIfUserIsLoggedIn()
+    enableLocationServices()
 //    signOut()
   }
   
@@ -46,6 +48,36 @@ class HomeController : UIViewController {
       try Auth.auth().signOut()
     } catch {
       print("Debug : error signing out ")
+    }
+  }
+}
+
+  //MARK: - LocationServices
+extension HomeController : CLLocationManagerDelegate {
+  func enableLocationServices() {
+    locationManager.delegate = self
+    
+    switch locationManager.authorizationStatus {
+    case .notDetermined:
+      locationManager.requestWhenInUseAuthorization() // 이걸 먼저 해야한다
+      print("Debug : not determined")
+    case .restricted , .denied:
+      break
+    case .authorizedAlways :
+      print("Debug : auth always")
+      locationManager.startUpdatingLocation()
+      locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    case .authorizedWhenInUse :
+      print("Debug : auth when in use")
+      locationManager.requestAlwaysAuthorization()
+    default:
+      break
+    }
+  }
+
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    if manager.authorizationStatus == .authorizedWhenInUse {
+      locationManager.requestAlwaysAuthorization() // 한번 사용 허락을 누르면 그다음 바로 계속 허락을 할꺼냐는 request를 보낸다.
     }
   }
 }
